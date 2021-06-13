930e8b1d9f14e6ba3399e92bc0558a779ab2d6ea
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-13 23:54:11 +0530

    Condition checks in UC4

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,16 +1,24 @@
+#!/bin/bash -x
 echo "Welcome to flip coin simulator"
 HeadCount=0
 TailCount=0
 while [ $HeadCount -le 20 ] || [ $TailCount -le 20 ]
 do
 random=$((RANDOM%2))
-	if [ $random -eq 0 ]
+	if [ $HeadCount -eq $TailCount ]
 	then
-        	echo "Head wins"
-		HeadCount=$(($HeadCount+1))
-	else
-        	echo "tail wins"
-		TailCount=$(($TailCount+1))
+		if [ $random -eq 0 ]
+		then
+        		echo "Head wins"
+			HeadCount=$(($HeadCount+1))
+		else
+        		echo "tail wins"
+			TailCount=$(($TailCount+1))
+		fi
+	diff=`echo $HeadCount $TailCount | awk '{print $1 - $2}'`
+	elif [ $diff -eq 2 ]
+	then
+		break
 	fi
 done
 
