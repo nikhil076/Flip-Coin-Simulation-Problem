5f37a02ece3f93c2fa901101b961e5c04c10b698
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-13 23:29:26 +0530

    added count of wins or loss

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,8 +1,18 @@
 echo "Welcome to flip coin simulator"
+HeadCount=0
+TailCount=0
+for i in {1..10}
+do
 random=$((RANDOM%2))
-if [ $random -eq 0 ]
-then
-        echo "Head wins"
-else
-        echo "tail wins"
-fi
+	if [ $random -eq 0 ]
+	then
+        	echo "Head wins"
+		HeadCount=$(($HeadCount+1))
+	else
+        	echo "tail wins"
+		TailCount=$(($TaailCount+1))
+	fi
+done
+
+echo $HeadCount
+echo $TailCount
